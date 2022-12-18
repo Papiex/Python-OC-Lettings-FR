b25f184571dbc8f47502ab5c616ec1040d7b01db
Papiexx <boutdepapier27@outlook.fr>
2022-12-18 14:07:06 +0100

    add index test for oc lettings site

--- oc_lettings_site/tests/units/tests.py
+++ oc_lettings_site/tests/units/tests.py
@@ -1,7 +1,10 @@
+import pytest
 
 
-def test_circle():
-    """for setup circle CI"""
-    test = 'ok'
+@pytest.mark.django_db
+def test_index_should_return_200(client):
+    response = client.get("")
+    response_data = response.content.decode()
 
-    assert test == 'ok'
+    assert response.status_code == 200
+    assert "Welcome to Holiday Homes" in response_data
